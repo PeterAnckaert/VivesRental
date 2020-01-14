3bfae443c37a2ff6acdc50404c0c6a14db0b3832
Peter Anckaert <r0780531@student.vives.be>
2020-01-14 20:11:33 +0100

    Checkout customer en article OK

--- VivesRental.WebApp/Controllers/ShopController.cs
+++ VivesRental.WebApp/Controllers/ShopController.cs
@@ -1,21 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using VivesRental.Model;
+using VivesRental.Repository.Includes;
+using VivesRental.Services.Contracts;
+using VivesRental.WebApp.Models;
 
 namespace VivesRental.WebApp.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly IArticleService _articleService;
+        private readonly ICustomerService _customerService;
+        private readonly IOrderService _orderService;
+        private readonly IOrderLineService _orderLineService;
+        private static readonly CheckOutViewModel CheckOutViewModel = new CheckOutViewModel();
+
+        public ShopController(IArticleService articleService, ICustomerService customerService, IOrderService orderService, IOrderLineService orderLineService)
+        {
+            _articleService = articleService;
+            _customerService = customerService;
+            _orderService = orderService;
+            _orderLineService = orderLineService;
+        }
+
         public IActionResult CheckIn()
         {
             return View();
         }
         public IActionResult CheckOut()
         {
-            return View();
+            CheckOutViewModel.Customers = _customerService.All().OrderBy(c => c.LastName).ThenBy(c => c.FirstName);
+            CheckOutViewModel.Articles = _articleService.GetAvailableArticles(new ArticleIncludes { Product = true }).OrderBy(a => a.Product.Name);
+            return View(CheckOutViewModel);
         }
 
+        public IActionResult SelectCustomer(Guid id)
+        {
+            CheckOutViewModel.SelectedCustomer = _customerService.Get(id);
+            return RedirectToAction("CheckOut");
+        }
+        
+        public IActionResult SelectArticle(Guid id)
+        {
+            var article = _articleService.Get(id, new ArticleIncludes {Product = true});
+            CheckOutViewModel.SelectedArticles.Add(article);
+            return RedirectToAction("CheckOut");
+        }
     }
 }
---
+++ VivesRental.WebApp/Models/CheckOutViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using VivesRental.Model;
+
+namespace VivesRental.WebApp.Models
+{
+    public class CheckOutViewModel
+    {
+        public CheckOutViewModel()
+        {
+            SelectedArticles = new List<Article>();
+        }
+
+        public IOrderedEnumerable<Customer> Customers { get; set; }
+        public IOrderedEnumerable<Article> Articles { get; set; }
+        public Customer SelectedCustomer { get; set; }
+        public IList<Article> SelectedArticles { get; set; }
+    }
+}
